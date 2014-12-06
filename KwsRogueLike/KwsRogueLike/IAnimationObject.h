b9212c97c3f49ad1ac9c740499fccf9f1cfcce5d
Kyoro_p <longcorridor@outlook.jp>
2014-12-06 14:17:38 +0900

    Draw関係のインターフェース追加

---
+++ KwsRogueLike/KwsRogueLike/IAnimationObject.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+class IAnimationObject
+{
+public:
+	virtual int GetAnimationHandle() = 0;
+	virtual ~IAnimationObject(){};
+};
---
+++ KwsRogueLike/KwsRogueLike/IDrawObject.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+class IDrawObject
+{
+public:
+	enum ImageType
+	{
+		PLAYER,
+		SWORD,
+	};
+
+	virtual int GetLayer() = 0;
+	virtual ImageType GetImageType() = 0;
+	virtual int GetX() = 0;
+	virtual int GetY() = 0;
+
+	virtual ~IDrawObject(){};
+};
